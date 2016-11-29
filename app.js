041ff066fa209e029901f83c1584f6d27fdbaadc
Daniel Obembe <ayo.obembe@gmail.com>
2016-11-29 14:43:44 +0100

    converted site into express app. reorganized pages.

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/concept.html'));
+});
+
+app.get('/concept.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/concept.html'));
+});
+
+app.get('/exhibitions.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/exhibitions.html'));
+});
+
+app.get('/artists.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/artists.html'));
+});
+
+app.get('/store.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/store.html'));
+});
+
+app.get('/news.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/news.html'));
+});
+
+app.listen(3000, function() {
+  console.log("SketchedSpace: listening on port 3000.")
+})
